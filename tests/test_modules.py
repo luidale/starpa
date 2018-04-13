265ef15ee11284f1a1487e33c35ea6074a893330
Hannes Luidalepp <luidale@gmail.com>
2018-04-13 17:58:30 +0200

    Added some tests vol2

---
+++ tests/test_modules.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Tests should be run from main folder
+'''
+
+import sys
+import os
+import shutil
+#allows to import from the src folder
+sys.path[0] = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.join("..","src")))
+import starpa.__main__ as starpa
+import unittest
+from docopt import docopt
+
+doc = starpa.__doc__
+config_file = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.join("data","config.txt")))
+
+
+class TestStarpa(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_01(self):
+        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.join("data","fq")))
+        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.join("data","output")))
+        args = docopt(doc, ["-s","trim","-e","trim","-c",config_file,\
+                            "-i",input_folder,"-o","tests/data/output"])
+        starpa.main(args)
+        shutil.rmtree("tests/data/output")
+        #self.assertTrue(run)
+
+    def test_02(self):
+        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.join("data","fq")))
+        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.join("data","output")))
+        args = docopt(doc, ["-s","trim","-e","quantify","-c",config_file,\
+                            "-i",input_folder,"-o","tests/data/output"])
+        starpa.main(args)
+        shutil.rmtree("tests/data/output")
+        #self.assertTrue(run)
+
+    def test_01a(self):
+        args = docopt(doc, ["--version"])
+        #print(args[1])
+        #run = starpa.main(["--start","align"])
+        self.assertEqual(args["--version"],True)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
