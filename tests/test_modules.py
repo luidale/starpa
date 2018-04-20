0fc193af928dc7389c4188c522269591b71a73ff
Hannes <luidale@gmail.com>
2018-04-20 12:23:27 +0200

    testing

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -50,71 +50,71 @@ class TestStarpa(unittest.TestCase):
                             "-i",input_folder,"-o","tests/data/output"])
         starpa.main(args)
 
-    def test_align(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output","trim")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","align","-e","align","-c",config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-
-    def test_sort(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output","align")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","sam_sort","-e","sam_sort","-c",config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-
-    def test_pseudoSE(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output","sam_sort")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","pseudoSE","-e","pseudoSE","-c",config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-
-    def test_identify(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output","pseudoSE")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","identify","-e","identify","-c",config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-
-    def test_cluster(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output","identify")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","cluster","-e","cluster","-c",config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-
-    def test_quantify(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output","cluster")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","quantify","-e","quantify","-c",config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-        shutil.rmtree("tests/data/output")
-        
-    def test_full(self):
-        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","fq")))
-        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           os.path.join("data","output")))
-        args = docopt(doc, ["-s","trim","-e","quantify", "-c", config_file,\
-                            "-i",input_folder,"-o","tests/data/output"])
-        starpa.main(args)
-        shutil.rmtree("tests/data/output")
-        #self.assertTrue(run)
+##    def test_align(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output","trim")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","align","-e","align","-c",config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##
+##    def test_sort(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output","align")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","sam_sort","-e","sam_sort","-c",config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##
+##    def test_pseudoSE(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output","sam_sort")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","pseudoSE","-e","pseudoSE","-c",config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##
+##    def test_identify(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output","pseudoSE")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","identify","-e","identify","-c",config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##
+##    def test_cluster(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output","identify")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","cluster","-e","cluster","-c",config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##
+##    def test_quantify(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output","cluster")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","quantify","-e","quantify","-c",config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##        shutil.rmtree("tests/data/output")
+##        
+##    def test_full(self):
+##        input_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","fq")))
+##        output_folder = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+##                           os.path.join("data","output")))
+##        args = docopt(doc, ["-s","trim","-e","quantify", "-c", config_file,\
+##                            "-i",input_folder,"-o","tests/data/output"])
+##        starpa.main(args)
+##        shutil.rmtree("tests/data/output")
+##        #self.assertTrue(run)
 
     def test_01a(self):
         args = docopt(doc, ["--version"])
