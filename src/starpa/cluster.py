0f5387153a5a4028ee945d6bf83825002d41e5a1
Hannes Luidalepp <luidale@gmail.com>
2018-04-23 11:48:32 +0200

    correct cluster.py bedtools merge

--- src/starpa/cluster.py
+++ src/starpa/cluster.py
@@ -723,7 +723,7 @@ class cluster():
             settings["bedtools_call"], "merge",
             "-i",os.path.join(settings["--output"],"cluster","contigs_meta",\
                               "combined_contigs_meta.BED"),
-            "-s", "-c", "5,6", "-o", "disttinct,distinct",
+            "-s", "-c", "5,6", "-o", "distinct,distinct",
             ">", os.path.join(settings["--output"],"cluster","contigs_meta",\
                               "metacontigs.BED")
             )
