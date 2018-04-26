b992cd916667a5c804e1cb17ed5a975c33f4dc8a
Luidale <luidale@gmail.com>
2018-04-27 00:22:44 +0200

    updated quantify.py

--- src/starpa/quantify.py
+++ src/starpa/quantify.py
@@ -107,7 +107,7 @@ class quantify():
                 strand = "-"
             pp_pos_list[strand], pp_name_list[strand] = \
                                       self.genes_to_positions4(pp_list[strand])
-
+        print(pp_pos_list)
         #GET DICTIONARY FOR PP-S
         print("\tMake pp dictionary")
         pp_data = self.make_pp_dictionary(settings,first_task)
@@ -186,7 +186,8 @@ class quantify():
 
     def genes_from_BED(self,annotation_file):
         '''
-        Creats dictionary for chromosomes containing lists with gene information
+        Creats dictionary for chromosomes containing lists with gene information. Info comes
+        from BED so it is 0 based end exclusiv.
         '''
         #print("\tCreate gene list")
         gene_list = {}
@@ -207,12 +208,17 @@ class quantify():
 
     def genes_to_positions4(self,gene_list):
         '''
-        Creates dictionary with positions of the genes refering to the name of the genes
+        Creates dictionary with positions of the genes refering to the name of the genes.
+        Input: 0-based end exclusive
+        Output: 0-based and end inclusive
         '''
         #print("\tAdding genes to positions")
         gene_dict = {}
         single_pos_element = set()
         for chrom in gene_list:
+            '''creates dictionary with start and end positions of the elements
+            {1:{1}, 10:{1}, 15:{2}, 20:{2}, 8:{3}, 12:{3}}
+            '''    
             for gene in gene_list[chrom][1:]:
                 if chrom not in gene_dict:
                     gene_dict[chrom] = {}
@@ -229,7 +235,11 @@ class quantify():
                 else:
                     gene_dict[chrom][int(gene[1])-1].add(gene[8]) 
 
-            #correct overlaping elements
+            '''correct overlaping elements
+            #if elements overlap and the one elements starts in the middle of the other
+            #then the elements get cross labeled in the positions
+            {1:{1}, 8:{1,3}, 10:{1,3}, 12:{3}, 15:{2}, 20:{2}}
+            '''
             genes_started = set()
             for x in sorted(gene_dict[chrom]):
                 #if no started elements
@@ -473,9 +483,9 @@ class quantify():
                     strand = "-"
                 else:
                     strand = "+"
-                #SAM coordination system (1-based, end iclusive) converted to 0-based end exclusive
+                #SAM coordination system (1-based) converted to 0-based end inclusive
                 start = int(line_split[3])-1
-                end = start + abs(int(line_split[5].strip("M")))
+                end = start + abs(int(line_split[5].strip("M")))-1
                 chrom = line_split[2]
                 #matching reads to genes
                 gene_list_lib = self.reads_to_genes4(line,gene_list_lib,gene_pos_list,\
