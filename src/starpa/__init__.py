00810fd2098acbd3a4f76c6d5cc80b784649051b
Luidale <luidale@gmail.com>
2018-05-04 21:39:09 +0200

    Updated version number

--- src/starpa/__init__.py
+++ src/starpa/__init__.py
@@ -1,6 +1,6 @@
 """starpa - Stable RNA processing product analyzer"""
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Hannes Luidalepp <luidale@gmail.com>'
 __all__ = []
 __license__ = 'The GNU General Public License v3.0 (GPL3) - http://www.gnu.org/copyleft/gpl.html'
