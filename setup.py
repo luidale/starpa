c8d423f2c3f02a5de18fe93fef68d8be0c7606be
Luidale <luidale@gmail.com>
2018-05-04 22:07:57 +0200

    corrected setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: Implementation :: PyPy3',
+		'Programming Language :: Python :: Implementation :: PyPy',
 		'Environment :: Console',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
